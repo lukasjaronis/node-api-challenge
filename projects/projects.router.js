da9cd420cd23f5068a92c657bc07e0e2a29e0fd4
Lukas Jaronis <jaronisluke13@gmail.com>
2020-01-17 10:44:20 -0600

    POST for projects, router, done

---
+++ projects/projects.router.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const projectModel = require('../data/helpers/projectModel');
+const { validateProjectID, validateProjectBody } = require('../middleware/middleware');
+
+// GET'ting all projects to display
+
+router.get('/', (request, response) => {
+    projectModel
+    .get()
+    .then(result => {
+        response.status(200).json(result)
+    })
+    .catch(error => {
+        response.status(500).json({
+            errorMessage: `There was an error retrieving projects. ${error}`
+        })
+    })
+})
+
+// GET'ting projects by ID
+
+router.get('/:id', validateProjectID, (request, response) => {
+    projectModel
+    .get(request.params.id)
+    .then(result => {
+        response.status(200).json(result)
+    })
+    .catch(error => {
+        response.status(500).json({
+            errorMessage: `There was an error retrieving project information. ${error}`
+        })
+    })
+})
+
+// GET'ting projects actions
+
+router.get('/:id/actions', validateProjectID, (request, response) => {
+    projectModel.getProjectActions(request.params.id)
+    .then(result => {
+        response.status(200).json(result)
+    })
+    .catch(error => {
+        response.status(500).json({
+            errorMessage: `There was an error retrieving project information. ${error}`
+        })
+    })
+})
+
+// POST
+
+router.post('/', validateProjectBody, (request, response) => {
+    projectModel
+    .insert(request.body)
+    .then(result => {
+        response.status(201).json(result)
+    })
+    .catch(error => {
+        response.status(500).json({
+            errorMessage: `There was an error posting your request. ${error}`
+        })
+    })
+})
